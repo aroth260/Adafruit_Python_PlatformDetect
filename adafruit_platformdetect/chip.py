50fe249ef0a323570280bacb1d759f5182a515af
aroth260 <51916103+aroth260@users.noreply.github.com>
2022-03-18 10:37:55 -0400

    Update chip.py

--- adafruit_platformdetect/chip.py
+++ adafruit_platformdetect/chip.py
@@ -188,6 +188,9 @@ class Chip:
         if self.detector.check_dt_compatible_value("sun8i-a33"):
             return chips.A33
 
+        if self.detector.check_dt_compatible_value("rockchip,rk3568"):
+            return chips.RK3568
+
         if self.detector.check_dt_compatible_value("rockchip,rk3308"):
             return chips.RK3308
 
